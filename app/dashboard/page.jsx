1b791ac57eda96672f7a86644c693d760e4fa0b1
LazyTuna <spitfire_1@live.com>
2024-03-21 22:30:27 -0500

    updated dashboard

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,26 @@
+import Card from "../ui/dashboard/card/card"
+import Chart from "../ui/dashboard/chart/chart"
+import styles from "../ui/dashboard/dashboard.module.css"
+import Rightbar from "../ui/dashboard/rightbar/rightbar"
+import Transactions from "../ui/dashboard/transactions/transactions"
+
 const Dasboard = () => {
     return (
-        <div>Dasboard</div>
-    )
-}
+        <div className={styles.wrapper}>
+            <div className={styles.main}>
+                <div className={styles.cards}>
+                    <Card />
+                    <Card />
+                    <Card />
+                </div>
+                <Transactions />
+                <Chart />
+            </div>
+            <div className={styles.side}>
+                <Rightbar />
+            </div>
+        </div>
+    );
+};
 
 export default Dasboard
